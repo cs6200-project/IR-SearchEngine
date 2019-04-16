64743469f16153d6c7757adc50c2cf68bf79db38
jialinNEU <gao.j@husky.neu.edu>
2019-04-16 15:01:16 -0400

    phase 3: first 8 runs evaluation

--- phase3/evaluation.py
+++ phase3/evaluation.py
@@ -5,11 +5,37 @@ from collections import defaultdict
 import os
 
 RELEVANCE = os.path.abspath(os.path.join(os.getcwd(), "../test-collection/cacm.rel.txt"))
-RUNRESULT = os.path.abspath(os.path.join(os.getcwd(), "../results/***/***.txt"))
-OUTPUT = os.path.abspath(os.path.join(os.getcwd(), "../results/phase3/***.txt"))
+
+# phase 1 - task 1
+BM25_RESULT = os.path.abspath(os.path.join(os.getcwd(), "../results/phase1_task1_res/bm25.txt"))
+BM25_OUTPUT = os.path.abspath(os.path.join(os.getcwd(), "../results/phase3/bm25_evaluation.txt"))
+
+LUCENE_RESULT = os.path.abspath(os.path.join(os.getcwd(), "../results/phase1_task1_res/Lucene result.txt"))
+LUCENE_OUTPUT = os.path.abspath(os.path.join(os.getcwd(), "../results/phase3/lucene_evaluation.txt"))
+
+TFIDF_RESULT = os.path.abspath(os.path.join(os.getcwd(), "../results/phase1_task1_res/tfidf.txt"))
+TFIDF_OUTPUT = os.path.abspath(os.path.join(os.getcwd(), "../results/phase3/tfidf_evaluation.txt"))
+
+Q_LIKELIHOOD_RESULT = os.path.abspath(os.path.join(os.getcwd(), "../results/phase1_task1_res/query_likelihood_model.txt"))
+Q_LIKELIHOOD_OUTPUT = os.path.abspath(os.path.join(os.getcwd(), "../results/phase3/query_likelihood_evaluation.txt"))
+
+# phase 1 - task 2
+REL_FEEDBACK_RESULT = os.path.abspath(os.path.join(os.getcwd(), "../results/phase1_task2_res/pseudo relevance feedback.txt"))
+REL_FEEDBACK_OUTPUT = os.path.abspath(os.path.join(os.getcwd(), "../results/phase3/rel_feedback_evaluation.txt"))
+
+STEM_QUERY_RESULT = os.path.abspath(os.path.join(os.getcwd(), "../results/phase1_task2_res/query time stemming.txt"))
+STEM_QUERY_OUTPUT = os.path.abspath(os.path.join(os.getcwd(), "../results/phase3/stem_query_evaluation.txt"))
+
+# phase 1 - task 3
+BM25_STOPPING_RESULT = os.path.abspath(os.path.join(os.getcwd(), "../results/phase1_task3_res/bm25_stopping.txt"))
+BM25_STOPPING_OUTPUT = os.path.abspath(os.path.join(os.getcwd(), "../results/phase3/bm25_stopping_evaluation.txt"))
+
+TFIDF_STOPPING_RESULT = os.path.abspath(os.path.join(os.getcwd(), "../results/phase1_task3_res/tfidf_stopping.txt"))
+TFIDF_STOPPING_OUTPUT = os.path.abspath(os.path.join(os.getcwd(), "../results/phase3/tfidf_stopping_evaluation.txt"))
 
 relevance_dict = defaultdict(list)
 query_result_dict = defaultdict(list)
+query_result_info_dict = defaultdict(list)
 
 
 # read relevance judgments from cacm.rel.txt
@@ -29,7 +55,16 @@ def read_query_results(filename):
         lines = f.readlines()
         for line in lines:
             l = line.strip().split(" ")
+            if len(l) == 1:
+                continue
+
             q_id = l[0]
+            doc_id = l[2]
+            rank = l[3]
+            score = l[4]
+            system = l[5]
+            tup = ("Q0", doc_id, rank, score, system)
+            query_result_info_dict[q_id].append(tup)
             if q_id in relevance_dict.keys():
                 doc_id = l[2]
                 query_result_dict[q_id].append(doc_id)
@@ -136,25 +171,64 @@ def calc_MRR(relevance_dict, query_result_dict):
     return MRR
 
 
-def evaluation(output=""):
+def evaluation(output):
     recall = calc_recall(relevance_dict, query_result_dict)
-    print("Recall: ", str(recall))
     precision = calc_precision(relevance_dict, query_result_dict)
-    print("Precision: ", str(precision))
+
     precision_at_5 = calc_precision_at_rank(relevance_dict, query_result_dict, 5)
     precision_at_20 = calc_precision_at_rank(relevance_dict, query_result_dict, 20)
-    print("P@5: ", precision_at_5)
-    print("P@20: ", precision_at_20)
+
     MAP = calc_MAP(relevance_dict, query_result_dict)
-    print("MAP: ", MAP)
     MRR = calc_MRR(relevance_dict, query_result_dict)
-    print("MRR", MRR)
+
+    with open(output, "w", encoding="utf-8") as f:
+        f.write("query_id,  literal,    doc_id, rank,   score,  system, recall, precision" + "\n")
+        for q_id, info_list in query_result_info_dict.items():
+            r = recall[q_id]
+            p = precision[q_id]
+            for idx, tup in enumerate(info_list):
+                if len(r) == 0:
+                    continue
+                f.write(q_id + " ")
+                f.write(tup[0] + " ")
+                f.write(tup[1] + " ")
+                f.write(tup[2] + " ")
+                f.write(tup[3] + " ")
+                f.write(tup[4] + " ")
+                f.write(str(r[idx]) + " ")
+                f.write(str(p[idx]) + " ")
+                f.write("\n")
+            f.write("p@5: " + str(precision_at_5) + ", p@20: " + str(precision_at_20) + "\n")
+        f.write("MAP: " + str(MAP) + ", MRR: " + str(MRR) + "\n")
 
 
 def main():
     read_relevance_judgments(RELEVANCE)
-    read_query_results(RUNRESULT)
-    evaluation()
+
+    # phase 1 - task 1 evaluation
+    read_query_results(BM25_RESULT)
+    evaluation(BM25_OUTPUT)
+    read_query_results(LUCENE_RESULT)
+    evaluation(LUCENE_OUTPUT)
+    read_query_results(TFIDF_RESULT)
+    evaluation(TFIDF_OUTPUT)
+    read_query_results(Q_LIKELIHOOD_RESULT)
+    evaluation(Q_LIKELIHOOD_OUTPUT)
+
+    # phase 1 - task 2 evaluation
+    read_query_results(REL_FEEDBACK_RESULT)
+    evaluation(REL_FEEDBACK_OUTPUT)
+    read_query_results(STEM_QUERY_RESULT)
+    evaluation(STEM_QUERY_OUTPUT)
+
+    # phase 1 - task 3 evaluation
+    read_query_results(BM25_STOPPING_RESULT)
+    evaluation(BM25_STOPPING_OUTPUT)
+    read_query_results(TFIDF_STOPPING_RESULT)
+    evaluation(TFIDF_STOPPING_OUTPUT)
+
+    # phase 3 evaluation
+
 
 
 main()
